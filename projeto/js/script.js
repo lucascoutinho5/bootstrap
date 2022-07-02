9c0057e1bd745da5131763c05e3018e762c241be
tecti <tecti@senai.ti>
2022-07-01 21:41:31 -0300

    aula dia 01

--- projeto/js/script.js
+++ projeto/js/script.js
@@ -54,3 +54,22 @@ function validarFormulario(){
 	
 	
 }
+function pesquisa(){
+	var input,fltro,menu,links;
+	var menuItens = [];
+	input= document.getElementById("pesquisa");
+	filtro=input.value.toUpperCase();
+	menu=document.getElementById("menu");
+	menuItens=menu.getElementsByTagName("li");
+	for(var i=0;i<menuItens.length;i++){
+		links=menuItens[i].getElementsByTagName("a")[0];
+		if(links.innerHTML.toUpperCase().indexOf(filtro)>-1){
+		menuItens[i].style.display="";	
+			
+		}else{
+			menuItens[i].style.display="none";	
+		}
+		
+	}
+	
+}
